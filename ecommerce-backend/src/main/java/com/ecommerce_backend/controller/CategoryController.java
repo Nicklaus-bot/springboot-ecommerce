418bb3965a593f990bd96b4be28535dae7a6b903
Sidharth Mohan <mohansidharth00@gmail.com>
2026-06-29 11:42:02 +0530

    added first category api to the project

---
+++ ecommerce-backend/src/main/java/com/ecommerce_backend/controller/CategoryController.java
@@ -0,0 +1,23 @@
+package com.ecommerce_backend.controller;
+
+import com.ecommerce_backend.model.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getCategories(){
+        return categories;
+    }
+
+
+
+
+}
